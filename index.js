d42510cdb9df03d547f4fe9252688345d9b9f5b3
Doug Newman <dougnewman@gmail.com>
2018-08-02 08:08:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function fetchBooks() {
   //write fetch request to the Game of Thrones API
+  
+fetch("https://anapioficeandfire.com/api/books")
+.then(resp => resp.json())
+.then(json => renderBooks(json));
+  
+
 }
 
 function renderBooks(json) {
@@ -14,3 +20,8 @@ function renderBooks(json) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
 })
+
+
+function fithBook () {
+  
+}
